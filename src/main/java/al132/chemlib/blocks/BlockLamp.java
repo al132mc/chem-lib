58871835c618533867746ec91bc81a6f9e0368cb
Alex Bertram <apbertram1@gmail.com>
2020-08-24 09:47:28 -0500

    v19

--- src/main/java/al132/chemlib/blocks/BlockLamp.java
+++ src/main/java/al132/chemlib/blocks/BlockLamp.java
@@ -1,6 +1,7 @@
 package al132.chemlib.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 public class BlockLamp extends Block {
@@ -8,6 +9,7 @@ public class BlockLamp extends Block {
         super(Block.Properties
                 .create(Material.EARTH)
                 .hardnessAndResistance(2.0f)
+                .sound(SoundType.GLASS)
                 .lightValue(15));
         setRegistryName("lamp_" + name);
         ModBlocks.blocks.add(this);
